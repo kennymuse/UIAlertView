cfbd58fe7dcc5ba927d6bd8f4a75c2db8dbe96db
Enrico De Michele <kennymuse@gmail.com>
2015-10-28 13:15:38 +0100

    Initial Commit

---
+++ UIAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIAlertView
+//
+//  Created by Enrico De Michele on 28/10/15.
+//  Copyright © 2015 Enrico De Michele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAlertView
+//
+//  Created by Enrico De Michele on 28/10/15.
+//  Copyright © 2015 Enrico De Michele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
